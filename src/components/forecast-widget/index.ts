d492790616eba5b68ddb32e16c661f6ee42d65b8
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2019-08-19 18:32:45 +0200

    Test compatibility with my Vue setup and vue-chartjs.

--- src/components/forecast-widget/index.ts
+++ src/components/forecast-widget/index.ts
@@ -6,10 +6,37 @@ import "./forecast-widget.scss";
 
 import StoreMixin from "../../mixins/store.mixin";
 
+import { Line, mixins as chartMixins } from "vue-chartjs";
+
 @Component({
-  mixins: [template],
+  mixins: [
+    template,
+    chartMixins.reactiveProp
+  ],
+  components: {
+    Line,
+  }
 })
 export default class ForecastWidget extends mixins(StoreMixin) {
+  /*************************************************/
+  /* PROPERTIES */
+  /*************************************************/
+  chartdata: {
+    labels: ["January", "February"],
+    datasets: [
+      {
+        label: "Data One",
+        backgroundColor: "#f87979",
+        data: [40, 20]
+      }
+    ]
+  };
+
+  options: {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
   /*************************************************/
   /* COMPUTED'S */
   /*************************************************/
